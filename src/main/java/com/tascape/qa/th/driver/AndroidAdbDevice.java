d668243035a57fccd493075356e25ac52f2e33d9
Linsong Wang <tascape@gmail.com>
2015-10-19 19:08:34 -0700

    update pom

--- src/main/java/com/tascape/qa/th/driver/AndroidAdbDevice.java
+++ src/main/java/com/tascape/qa/th/driver/AndroidAdbDevice.java
@@ -7,17 +7,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.imageio.ImageIO;
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FrameGrabber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -95,56 +89,6 @@ public class AndroidAdbDevice extends EntityDriver {
         }
         return events;
     }
-//
-//    /**
-//     * Some jar files are platform specific, please see javacv (https://github.com/bytedeco/javacv) for more information.
-//     * @param screenRecordFile
-//     *
-//     * @return
-//     *
-//     * @throws java.io.IOException
-//     * @throws org.bytedeco.javacv.FrameGrabber.Exception
-//     * @throws java.lang.InterruptedException
-//     */
-//    public List<Long> getScreenUpdates(String screenRecordFile) throws IOException, FrameGrabber.Exception,
-//            InterruptedException {
-//        File mp4 = this.getScreenRecord(screenRecordFile);
-//        LOG.debug("{}", mp4);
-//        List<Long> updates = new ArrayList<>();
-//
-//        Path p = Paths.get(System.getProperty("user.home"), "touch" + System.currentTimeMillis());
-//        File dir = p.toFile();
-//        dir.mkdirs();
-//        LOG.trace("Save frames to {}", dir);
-//
-//        FFmpegFrameGrabber g = new FFmpegFrameGrabber(mp4.getAbsolutePath());
-//        g.start();
-//
-//        BufferedImage previousImage = null;
-//        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-//            Frame f = g.grabFrame();
-//            if (f == null) {
-//                break;
-//            }
-//            BufferedImage currentImage = f.image.getBufferedImage();
-//            long ts = g.getTimestamp();
-//            if (previousImage != null) {
-//                if (!this.bufferedImagesEqual(previousImage, currentImage)) {
-//                    updates.add(ts);
-//                }
-//            }
-//
-//            File name = new File(dir, String.format("frame_%08d.png", i));
-//            ImageIO.write(currentImage, "png", name);
-//            previousImage = ImageIO.read(name);
-//        }
-//
-//        LOG.debug("total frames {}", g.getFrameNumber());
-//        LOG.debug("total length {}", g.getLengthInTime());
-//
-//        g.stop();
-//        return updates;
-//    }
 
     @Override
     public String getName() {
