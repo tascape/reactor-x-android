631c671684facff0fc080bc7c5f8914450d9e61a
Linsong Wang <tascape@gmail.com>
2015-10-20 09:38:08 -0700

    move lipermi in as a module

---
+++ uia-server/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -xe
+
+rm -fr libs || echo ""
+mkdir libs
+ls -l
+
+cp ../lipermi/target/lipermi-1.0.1.jar libs
+cp ../uia-client/target/thx-android-uiac-1.0.0.jar libs
+
+ant clean build
+
+rm  lipermi-*.jar
+rm thx-android-uiac-*.jar
+
+ls -l
+cd bin
+ls -l
+
+echo "adb push bundle.jar /data/local/tmp"
+echo "adb push uia-server.jar /data/local/tmp"
+echo "adb shell uiautomator runtest uia-server.jar bundle.jar -c com.android.uiautomator.stub.UiAutomatorRmiServer"
+echo "adb forward --remove tcp:local_port"
+echo "adb forward tcp:local_port tcp:8998"
+
