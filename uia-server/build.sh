c1211c79e8a0e786d0f0fc29ea6af10f79673729
Linsong Wang <tascape@gmail.com>
2015-11-25 14:30:49 -0800

    use new uia-server jars

--- uia-server/build.sh
+++ uia-server/build.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
-rm -fr libs || echo ""
-mkdir libs
+rm -fr bin
+rm -fr libs
+
 ls -l
+mkdir bin
+mkdir libs
 
 # update this as needed
 cp ../lipermi/target/thx-android-lipermi-1.0.3.jar libs
@@ -13,11 +16,15 @@ ant clean build
 rm thx-android-*.jar
 
 ls -l
-cd bin
-ls -l
+pushd bin
+  ls -l
 
-echo "adb push bundle.jar /data/local/tmp"
-echo "adb push uia-server.jar /data/local/tmp"
-echo "adb shell uiautomator runtest uia-server.jar bundle.jar -c com.android.uiautomator.stub.UiAutomatorRmiServer"
-echo "adb forward --remove tcp:8998"
-echo "adb forward tcp:8998 tcp:8998"
+  echo "adb push bundle.jar /data/local/tmp"
+  echo "adb push uia-server.jar /data/local/tmp"
+  echo "adb shell uiautomator runtest uia-server.jar bundle.jar -c com.android.uiautomator.stub.UiAutomatorRmiServer"
+  echo "adb forward --remove tcp:8998"
+  echo "adb forward tcp:8998 tcp:8998"
+popd
+
+cp bin/bundle.jar     ../uia-test/src/main/resources/uias/
+cp bin/uia-server.jar ../uia-test/src/main/resources/uias/
