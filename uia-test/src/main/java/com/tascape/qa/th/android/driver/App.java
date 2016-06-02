995ea718dcf4aa3332134dc9e01c3af96c5693ef
Linsong Wang <tascape@gmail.com>
2016-06-02 01:50:11 -0700

    1.1.15 update viewer

--- uia-test/src/main/java/com/tascape/qa/th/android/driver/App.java
+++ uia-test/src/main/java/com/tascape/qa/th/android/driver/App.java
@@ -23,8 +23,44 @@ import com.google.common.collect.Lists;
 import com.tascape.qa.th.Utils;
 import com.tascape.qa.th.driver.EntityDriver;
 import com.tascape.qa.th.exception.EntityDriverException;
+import com.tascape.qa.th.ui.SmartScroller;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -124,5 +160,252 @@ public abstract class App extends EntityDriver {
         device.clickByText(name);
         Utils.sleep(this.getLaunchDelayMillis(), "wait for app to launch");
         device.waitForIdle();
+    }
+
+    /**
+     * The method starts a GUI to let an user inspect element tree and take screenshot when the user is interacting
+     * with the app-under-test manually. Please make sure to set timeout long enough for manual interaction.
+     *
+     * @param timeoutMinutes timeout in minutes to fail the manual steps
+     *
+     * @throws Exception if case of error
+     */
+    public void interactManually(int timeoutMinutes) throws Exception {
+        LOG.info("Start manual UI interaction");
+        long end = System.currentTimeMillis() + timeoutMinutes * 60000L;
+
+        AtomicBoolean visible = new AtomicBoolean(true);
+        AtomicBoolean pass = new AtomicBoolean(false);
+        String tName = Thread.currentThread().getName() + "m";
+        SwingUtilities.invokeLater(() -> {
+            String info = device.getProductDetail() + "" + device.getSerial();
+            JDialog jd = new JDialog((JFrame) null, "Manual Device UI Interaction - " + info);
+            jd.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+
+            JPanel jpContent = new JPanel(new BorderLayout());
+            jd.setContentPane(jpContent);
+            jpContent.setPreferredSize(new Dimension(1088, 828));
+            jpContent.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+            JPanel jpInfo = new JPanel();
+            jpContent.add(jpInfo, BorderLayout.PAGE_START);
+            jpInfo.setLayout(new BorderLayout());
+            {
+                JButton jb = new JButton("PASS");
+                jb.setForeground(Color.green.darker());
+                jb.setFont(jb.getFont().deriveFont(Font.BOLD));
+                jpInfo.add(jb, BorderLayout.LINE_START);
+                jb.addActionListener(event -> {
+                    pass.set(true);
+                    jd.dispose();
+                    visible.set(false);
+                });
+            }
+            {
+                JButton jb = new JButton("FAIL");
+                jb.setForeground(Color.red);
+                jb.setFont(jb.getFont().deriveFont(Font.BOLD));
+                jpInfo.add(jb, BorderLayout.LINE_END);
+                jb.addActionListener(event -> {
+                    pass.set(false);
+                    jd.dispose();
+                    visible.set(false);
+                });
+            }
+
+            JLabel jlTimeout = new JLabel("xxx seconds left", SwingConstants.CENTER);
+            jpInfo.add(jlTimeout, BorderLayout.CENTER);
+            jpInfo.add(jlTimeout, BorderLayout.CENTER);
+            new SwingWorker<Long, Long>() {
+                @Override
+                protected Long doInBackground() throws Exception {
+                    while (System.currentTimeMillis() < end) {
+                        Thread.sleep(1000);
+                        long left = (end - System.currentTimeMillis()) / 1000;
+                        this.publish(left);
+                    }
+                    return 0L;
+                }
+
+                @Override
+                protected void process(List<Long> chunks) {
+                    Long l = chunks.get(chunks.size() - 1);
+                    jlTimeout.setText(l + " seconds left");
+                    if (l < 850) {
+                        jlTimeout.setForeground(Color.red);
+                    }
+                }
+            }.execute();
+
+            JPanel jpResponse = new JPanel(new BorderLayout());
+            JPanel jpProgress = new JPanel(new BorderLayout());
+            jpResponse.add(jpProgress, BorderLayout.PAGE_START);
+
+            JTextArea jtaResponse = new JTextArea();
+            jtaResponse.setEditable(false);
+            jtaResponse.setTabSize(4);
+            Font font = jtaResponse.getFont();
+            jtaResponse.setFont(new Font("Courier New", font.getStyle(), font.getSize()));
+            JScrollPane jsp = new JScrollPane(jtaResponse);
+            SmartScroller ss = new SmartScroller(jsp);
+            jpResponse.add(jsp, BorderLayout.CENTER);
+
+            JPanel jpScreen = new JPanel();
+            jpScreen.setMinimumSize(new Dimension(200, 200));
+            jpScreen.setLayout(new BoxLayout(jpScreen, BoxLayout.PAGE_AXIS));
+            JScrollPane jsp1 = new JScrollPane(jpScreen);
+            jpResponse.add(jsp1, BorderLayout.LINE_START);
+
+            JPanel jpJs = new JPanel(new BorderLayout());
+            JTextArea jtaJs = new JTextArea();
+            jpJs.add(new JScrollPane(jtaJs), BorderLayout.CENTER);
+
+            JSplitPane jSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, jpResponse, jpJs);
+            jSplitPane.setResizeWeight(0.88);
+            jpContent.add(jSplitPane, BorderLayout.CENTER);
+
+            JPanel jpLog = new JPanel();
+            jpLog.setBorder(BorderFactory.createEmptyBorder(5, 0, 0, 0));
+            jpLog.setLayout(new BoxLayout(jpLog, BoxLayout.LINE_AXIS));
+
+            JCheckBox jcbTap = new JCheckBox("Enable Click", null, false);
+            jpLog.add(jcbTap);
+            jpLog.add(Box.createHorizontalStrut(8));
+
+            JButton jbLogUi = new JButton("Log Screen");
+            jpResponse.add(jpLog, BorderLayout.PAGE_END);
+            {
+                jpLog.add(jbLogUi);
+                jbLogUi.addActionListener((ActionEvent event) -> {
+                    jtaResponse.setText("waiting for screenshot...");
+                    Thread t = new Thread(tName) {
+                        @Override
+                        public void run() {
+                            LOG.debug("\n\n");
+                            try {
+                                jtaResponse.setText("");
+                                jtaResponse.append(device.loadWindowHierarchy().root.toJson().toString(2));
+                                jtaResponse.append("\n");
+
+                                File png = device.takeDeviceScreenshot();
+                                BufferedImage image = ImageIO.read(png);
+
+                                int w = device.getDisplayWidth();
+                                int h = device.getDisplayHeight();
+
+                                BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+                                Graphics2D g2 = resizedImg.createGraphics();
+                                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                                    RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                                g2.drawImage(image, 0, 0, w, h, null);
+                                g2.dispose();
+
+                                JLabel jLabel = new JLabel(new ImageIcon(resizedImg));
+                                jpScreen.removeAll();
+                                jsp1.setPreferredSize(new Dimension(w + 30, h));
+                                jpScreen.add(jLabel);
+
+                                jLabel.addMouseListener(new MouseAdapter() {
+                                    @Override
+                                    public void mouseClicked(MouseEvent e) {
+                                        LOG.debug("clicked at {},{}", e.getPoint().getX(), e.getPoint().getY());
+                                        if (jcbTap.isSelected()) {
+                                            device.click(e.getPoint().x, e.getPoint().y);
+                                            jbLogUi.doClick();
+                                        }
+                                    }
+                                });
+                            } catch (Exception ex) {
+                                LOG.error("Cannot log screen", ex);
+                                jtaResponse.append("Cannot log screen");
+                            }
+                            jtaResponse.append("\n\n\n");
+                            LOG.debug("\n\n");
+
+                            jd.setSize(jd.getBounds().width + 1, jd.getBounds().height + 1);
+                            jd.setSize(jd.getBounds().width - 1, jd.getBounds().height - 1);
+                        }
+                    };
+                    t.start();
+                });
+            }
+            jpLog.add(Box.createHorizontalStrut(38));
+            {
+                JButton jbLogMsg = new JButton("Log Message");
+                jpLog.add(jbLogMsg);
+                JTextField jtMsg = new JTextField(10);
+                jpLog.add(jtMsg);
+                jtMsg.addFocusListener(new FocusListener() {
+                    @Override
+                    public void focusLost(final FocusEvent pE) {
+                    }
+
+                    @Override
+                    public void focusGained(final FocusEvent pE) {
+                        jtMsg.selectAll();
+                    }
+                });
+                jtMsg.addKeyListener(new KeyAdapter() {
+                    @Override
+                    public void keyPressed(java.awt.event.KeyEvent e) {
+                        if (e.getKeyCode() == java.awt.event.KeyEvent.VK_ENTER) {
+                            jbLogMsg.doClick();
+                        }
+                    }
+                });
+                jbLogMsg.addActionListener(event -> {
+                    Thread t = new Thread(tName) {
+                        @Override
+                        public void run() {
+                            String msg = jtMsg.getText();
+                            if (StringUtils.isNotBlank(msg)) {
+                                LOG.info("{}", msg);
+                                jtMsg.selectAll();
+                            }
+                        }
+                    };
+                    t.start();
+                    try {
+                        t.join();
+                    } catch (InterruptedException ex) {
+                        LOG.error("Cannot take screenshot", ex);
+                    }
+                    jtMsg.requestFocus();
+                });
+            }
+            jpLog.add(Box.createHorizontalStrut(38));
+            {
+                JButton jbClear = new JButton("Clear");
+                jpLog.add(jbClear);
+                jbClear.addActionListener(event -> {
+                    jtaResponse.setText("");
+                });
+            }
+
+            JPanel jpAction = new JPanel();
+            jpContent.add(jpAction, BorderLayout.PAGE_END);
+            jpAction.setLayout(new BoxLayout(jpAction, BoxLayout.LINE_AXIS));
+            jpJs.add(jpAction, BorderLayout.PAGE_END);
+
+            jd.pack();
+            jd.setVisible(true);
+            jd.setLocationRelativeTo(null);
+
+            jbLogUi.doClick();
+        });
+
+        while (visible.get()) {
+            if (System.currentTimeMillis() > end) {
+                LOG.error("Manual UI interaction timeout");
+                break;
+            }
+            Thread.sleep(500);
+        }
+
+        if (pass.get()) {
+            LOG.info("Manual UI Interaction returns PASS");
+        } else {
+            Assert.fail("Manual UI Interaction returns FAIL");
+        }
     }
 }
